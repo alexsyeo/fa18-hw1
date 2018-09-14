2cd29915ac469f4e6a6ec21d35d48dd6527c627f
alexsyeo <alexsyeo@comcast.net>
2018-09-14 10:14:20 -0700

    ay

--- foobar.rb
+++ foobar.rb
@@ -4,5 +4,9 @@ class Foobar
     # Class method, not an instance method
     # Call with `Foobar.baz`
     # Q4 CODE HERE
+    # sum = 0
+    # a.map {|a| a.to_i + 2 }.keep_if {|a| a < 12 && (a % 2 == 0)}.uniq.each {|a| sum += a}
+    # sum
+    a.map {|a| a.to_i + 2 }.keep_if {|a| a < 12 && (a % 2 == 0)}.uniq.sum
   end
 end
--- hw1.rb
+++ hw1.rb
@@ -1,16 +1,17 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
-
+  (a + b) ** 2
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
+  a.sort!.map {|a| a+1}
 
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
-
+  "#{first_name} #{last_name}"
 end
 
 def blockin_time(a)
@@ -49,4 +50,5 @@ def scrabble(word)
     z: 10,
   }
   # Q5 CODE HERE
+  word.chars.map {|c| values[c.to_sym]}.sum
 end
